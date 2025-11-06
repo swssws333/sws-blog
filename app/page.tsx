4e0bcdc4f52da878c131e86fb71eba0b82139357
wensheng <13757048+tsukihikari@user.noreply.gitee.com>
2025-11-06 17:08:50 +0800

    提交

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <div className={'bg-blue-300 px-4 py-2 text-center'}>
-        Welcome to Sun Wensheng's blog
+        Welcome to Sun Wensheng&apos;s blog
       </div>
     </div>
   )
